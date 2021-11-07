6a34baa32a52e2c2ffb4ade0e595a212fe35550e
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-11-07 14:46:32 +0200

    diff

---
+++ node_vs_js/diff.js
@@ -0,0 +1,13 @@
+//What are 3 things node.js has that the client side javascript doesn’t
+// have. And the reasons why.
+
+// 1. has require fs
+// 2. 
+// 3.
+
+// What are 3 things the client side javascript has that node.js doesn’t
+// have. And the reasons why.
+
+// 1. chrome v8
+// 2.
+// 3.
