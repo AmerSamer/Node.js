0f41d778c6660b938ecedf4712f155877584eec2
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-11-08 18:35:06 +0200

    Api

--- api/api.js
+++ api/api.js
@@ -1,36 +1,19 @@
-const axios = require('axios');
-const request = require('request');
+const { default: axios } = require('axios');
+const express = require('express');
+const app = express();
 
-async function getJokes() {
-    // try {
-    //     const response = await axios.get('https://api.chucknorris.io/jokes/random');
-    //     console.log(response.data.value);
-    // } catch (error) {
-    //     console.error(error);
-    // }
-}
-const getJokesReq = () => {
-    // const res = request('https://api.chucknorris.io/jokes/random', function (error, response, body) {
-    //      console.error('error:', error); // Print the error if one occurred
-    //      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //      console.log('body:', body); // Print the HTML for the Google homepage.
-    // });
-}
-const getJokes3Party = async () => {
-    var options = {
-        'method': 'GET',
-        'url': 'https://api.chucknorris.io/jokes/random',
-        'headers': {
-          'X-API-KEY': 'E6xwOg2BY4RbmiJogfyegrt746r7te',
-          'Content-Type': 'application/json'
-        }
-      };
-      const result = await axios(options);
-      console.log(result.data.value);
+const getJokesExpress = async () => {
+    const response = await axios.get('https://api.chucknorris.io/jokes/random')
+    app.get('/', (req, res) => {
+        return res.json(response.data.value);
+    });
+    app.listen(4000, () =>
+        console.log(`listening on port 4000!`),
+    );
 }
 
 module.exports = {
-    getJokes,
-    getJokesReq,
-    getJokes3Party
+    getJokesExpress
 };
+
+
--- api/app.js
+++ api/app.js
@@ -2,4 +2,5 @@ const jokes = require('./api.js');
 
 // const addJokes = jokes.getJokes()
 // const addJokes = jokes.getJokesReq()
-const addJokes = jokes.getJokes3Party()
+// const addJokes = jokes.getJokes3Party()
+const addJokes = jokes.getJokesExpress()
