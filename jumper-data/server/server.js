9d297e33e9a59c5f75b091e57d5cc6148c701e2c
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2021-11-14 17:37:43 +0200

    jumper details BACK

--- jumper-data/server/server.js
+++ jumper-data/server/server.js
@@ -7,32 +7,35 @@ const mongoose = require('mongoose');
 const itemsModel = require('./models/Items.model').itemsModel;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => {
     itemsModel.find({}, (err, data) => {
         if (err) throw err;
-        // return res.status(200).send( data+ ' AVG: ' +x(data))
         return res.status(200).send(data)
     })
 })
-// const x = (data)=>{
-//     let avg = (data[0].jump1 + data[0].jump2 + data[0].jump3) / 3
-//     return avg
-// }
+app.get('/:jumperId', (req, res) => {
+    const { jumperId } = req.params
+    itemsModel.findById(jumperId, (err, data) => {
+        if (err) throw err;
+        const total = (data.jump1 + data.jump2 + data.jump3) / 3
+        return res.status(200).send(data + ' AVG: ' + total)
+    })
+})
 app.delete('/:jumperId', (req, res) => {
-    const {jumperId} = req.params
+    const { jumperId } = req.params
     itemsModel.findByIdAndDelete(jumperId, (err, data) => {
         if (err) throw err;
         if (data) {
-            return res.status(200).json({success: data})
+            return res.status(200).json({ success: data })
         }
-        return res.status(400).json({itemNotFound: 'Item not found'})
+        return res.status(400).json({ itemNotFound: 'Item not found' })
     })
 })
 app.post('/', (req, res) => {
-    const {name, age, country , jump1 , jump2 , jump3 } = req.body
+    const { name, age, country, jump1, jump2, jump3 } = req.body
     const newItem = new itemsModel({
         name: name,
         age: age,
@@ -42,20 +45,20 @@ app.post('/', (req, res) => {
         jump3: jump3,
     });
     newItem.save()
-    return res.status(200).json({success: newItem})
+    return res.status(200).json({ success: newItem })
 })
 app.put('/:jumperId', (req, res) => {
-    const {jumperId} = req.params
-    const {jump1 , jump2 , jump3} = req.body;
-    itemsModel.findByIdAndUpdate(jumperId,{ jump1: jump1 , jump2: jump2 , jump3: jump3}, (err, data) => {
+    const { jumperId } = req.params
+    const { jump1, jump2, jump3 } = req.body;
+    itemsModel.findByIdAndUpdate(jumperId, { jump1: jump1, jump2: jump2, jump3: jump3 }, (err, data) => {
         if (err) throw err;
         if (data) {
-            return res.status(200).json({success: 'Updated Successfully'})
+            return res.status(200).json({ success: 'Updated Successfully' })
         }
-        return res.status(400).json({itemNotFound: 'Item not found'})
+        return res.status(400).json({ itemNotFound: 'Item not found' })
     })
 })
-mongoose.connect('mongodb://localhost/dbJumpers', {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+mongoose.connect('mongodb://localhost/dbJumpers', { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log('Connected to DB');
 });
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
